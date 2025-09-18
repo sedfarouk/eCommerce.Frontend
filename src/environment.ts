4dc341e0e8133cbe778f62798f42200c85c13cf5
Farouk Umar Sedick <sadikalhanssah@gmail.com>
2025-09-18 08:46:25 +0000

    frontend template for client UI

---
+++ src/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7080/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'http://localhost:5120/api/products/'
+  };
+  
